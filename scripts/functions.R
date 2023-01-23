03464826f0f2ca15627929fa9d03619b6bf246bf
sagesteppe <reedbenkendorf27@gmail.com>
2023-01-23 15:46:55 -0600

    king wilson now supports sf

--- scripts/functions.R
+++ scripts/functions.R
@@ -13,6 +13,7 @@
 #" )
 #' out <- WilsonCanHaveFun( data = df, outcome = 'Outcome' )
 #' @export
+
 WilsonCanHaveFun <- function(data, outcome, conf, ...){
   
   if(missing(conf)){conf <- 0.2}
@@ -21,22 +22,27 @@ WilsonCanHaveFun <- function(data, outcome, conf, ...){
     col_classes <- sapply(FUN = class, data)
     lgl_cols <- grep('logical', col_classes)
     
-      if(length(lgl_cols) > 1){
-        stop('No "success" columns specified to function, and input data frame contains more than one "logical" type column; please specify the "outcome" column')
-        } else {
-          outcome <- colnames(data)[lgl_cols] 
-          message('No column supplied to the "Success" argument, `', 
-                 substitute(outcome),  '` used as the outcome variable') } 
+    if(length(lgl_cols) > 1){
+      stop('No "success" columns specified to function, and input data frame contains more than one "logical" type column; please specify the "outcome" column')
+    } else {
+      outcome <- colnames(data)[lgl_cols] 
+      message('No column supplied to the "Success" argument, `', 
+              substitute(outcome),  '` used as the outcome variable') } 
   }
   
   outcome_col <- as.character(substitute(outcome))
-  outcome_var <- data[outcome_col] == T
   
+  inType <- class(data)
+  out <- if(length(grep('sf', inType)) == 0){
+    outcome_var <- data[outcome_col] == T 
+  } else{ 
+    data <- st_drop_geometry(data)
+    outcome_var <- data[outcome_col] == T
+  }
   prediction <- Hmisc::binconf(x = length(outcome_var[outcome_var==T]) , 
-          n = length(outcome_var), alpha = (1 - conf), ... )
-  prediction <-
-    data.frame(cbind(Variable = deparse(substitute(data)), prediction))
+                               n = length(outcome_var), alpha = (1 - conf), ... )
+  #  prediction <-
+  #    data.frame(cbind(Variable = deparse(substitute(data)), prediction))
   
   return(prediction)
 }
-
